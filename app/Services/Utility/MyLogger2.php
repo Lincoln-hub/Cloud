e571fba197436f16a25c858fe9be5c3cbf31061a
Lincoln-hub <72630978+Lincoln-hub@users.noreply.github.com>
2021-03-28 22:15:09 -0700

    loggly handler

--- app/Services/Utility/MyLogger2.php
+++ app/Services/Utility/MyLogger2.php
@@ -16,7 +16,7 @@ class MyLogger2 implements ILoggerService
         if(self::$logger == null)
         {
             self::$logger = new Logger('logger');
-            self::$logger->pushHandler(new StreamHandler('[LOGGLY TOKEN]/tag/cst323_logfile_heroku_upload',Logger::DEBUG));
+            self::$logger->pushHandler(new LogglyHandler('[LOGGLY TOKEN]/tag/cst323_logfile_heroku_upload',Logger::DEBUG));
         }
         return self::$logger;
     }
